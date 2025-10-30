c8752c09cac5836b92cb76ff4eb114276ec4de09
pedamallusurya <pedamallusuryaprakash@gmail.com>
2025-10-30 12:28:18 +0530

    FirstLwcCmp

--- force-app/main/default/lwc/myfirstLwcCmp/myfirstLwcCmp.js
+++ force-app/main/default/lwc/myfirstLwcCmp/myfirstLwcCmp.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class MyfirstLwcCmp extends LightningElement {}
+export default class MyfirstLwcCmp extends LightningElement {
+    empName = 'Rajesh';
+    empAge = 30;
+    Address = 'Bangalore,Karnataka';
+    isShow = false;
+    getAddress() {
+        console.log('Hello This');
+        alert('Welcome');
+        this.address = 'Hyderabad, Telangana';
+        this.isShow = true;
+    }
+}
